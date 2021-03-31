d644c273fee24009c49d5f7698768c86c89f9913
shreya-51 <48033781+shreya-51@users.noreply.github.com>
2021-03-31 10:40:30 -0400

    Added Hello_World source code file

---
+++ Hello_World.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(13, OUTPUT);    // sets the digital pin 13 as output
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // sets the digital pin 13 on
+  delay(50);            // waits
+  digitalWrite(13, LOW);  // sets the digital pin 13 off
+  delay(50);            // waits
+}
